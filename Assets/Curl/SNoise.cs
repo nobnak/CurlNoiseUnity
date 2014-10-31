a75bb9f8d757beb7c18599081da12df68fc60d67
Nakata Nobuyuki <nobu.nakata@gmail.com>
2014-10-31 20:15:51 +0900

    no message

---
+++ Assets/Curl/SNoise.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SNoise : MonoBehaviour {
+	public const int N_THREADS = 64;
+	public const int KERNEL_SNOISE = 0;
+
+	public const string SHADER_POS_IN = "PosIn";
+	public const string SHADER_POS_OUT = "PosOut";
+
+	public int n = 512;
+	public Vector4 speed;
+	public ComputeShader snoise;
+
+	private int _nGroups;
+	private RenderTexture _noiseTex;
+
+	void OnDisable() {
+		Release();
+	}
+	void Update() {
+		CheckInit();
+
+		snoise.SetVector(SHADER_SPEED, speed);
+		snoise.SetFloat(SHADER_TIME, Time.timeSinceLevelLoad);
+		snoise.SetTexture(KERNEL_SNOISE, SHADER_RESULT, _noiseTex);
+		_noiseTex.DiscardContents();
+		snoise.Dispatch(KERNEL_SNOISE, _nGroups, _nGroups, 1);
+
+		var m = renderer.sharedMaterial;
+		m.mainTexture = _noiseTex;
+	}
+
+	void CheckInit() {
+		if (_noiseTex != null && _noiseTex.width == n)
+			return;
+
+		Release();
+
+		_nGroups = n / N_THREADS;
+
+		_noiseTex = new RenderTexture(n, n, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+		_noiseTex.enableRandomWrite = true;
+		_noiseTex.Create();
+	}
+	void Release() {
+		if (_noiseTex != null)
+			_noiseTex.Release();
+	}
+}
---
+++ Assets/Curl/SNoise.shader
@@ -0,0 +1,40 @@
+﻿Shader "Custom/SNoise" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex;
+
+			struct Input {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+			struct vs2ps {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+			
+			vs2ps vert(Input IN) {
+				vs2ps OUT;
+				OUT.vertex = mul(UNITY_MATRIX_MVP, IN.vertex);
+				OUT.uv = IN.uv;
+				return OUT;
+			}
+			
+			float4 frag(vs2ps IN) : COLOR {
+				float4 c = tex2D(_MainTex, IN.uv);
+				return c;
+			}
+			ENDCG
+		}
+	} 
+}
